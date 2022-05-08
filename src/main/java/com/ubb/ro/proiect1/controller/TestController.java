9b6806d6572d9340523741aee4b21443cf9c703a
mihaim97 <marian.ionu97@gmail.com>
2022-05-08 13:50:26 +0300

    initial commit. Create project, add hibernate config, exclude spring jpa configuration .

---
+++ src/main/java/com/ubb/ro/proiect1/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.ubb.ro.proiect1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testController() {
+        return ResponseEntity.ok("{\"response\":\"ok\"}");
+    }
+
+}
---
+++ src/main/java/com/ubb/ro/proiect1/hibernate/HibernateConfiguration.java
@@ -0,0 +1,54 @@
+package com.ubb.ro.proiect1.hibernate;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        basicDataSource.setUrl("jdbc:sqlserver://serverdb197.database.windows.net:1433;database=onlineapp;user=proiect@serverdb197;password=postunivParola2022@;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+        return basicDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("ro.mihai.onlineapp.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties properties = new Properties();
+        //properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
+        return properties;
+    }
+
+
+
+
+
+}
